128a079b6b1ddc8a519ccdd5b1a622ae7d6c123b
Sanchit Khedkar <16sanchitkhedkar@gmail.com>
2025-03-15 15:26:47 +0530

    Update

--- 1.c
+++ 1.c
@@ -1,14 +1,11 @@
 /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.*/
 #include <stdio.h>
-unsigned int unitSum(unsigned int n)
-{
+int unitSum(int n){
     return (n*(n+1))/2;
 }
-int main(){
-    int n=1;
+int bruteForceSum(int n){
     int sum=0;
-    int actualSum = 3*unitSum((int)(999/3)) + 5*unitSum((int)(999/5)) - 15*unitSum((int)(999/15));
     while(n<1000){
         if((n%3==0) && (n%5==0)){
             sum+=n;
@@ -21,6 +18,12 @@ int main(){
         }
         n++;
     }
-    printf("%d\n", sum);
-    printf("%d\n", actualSum);
+    return sum;
+}
+int main(){
+    int loopSum = bruteForceSum(1);
+    int formulaSum = 3*unitSum((int)(999/3)) + 5*unitSum((int)(999/5)) - 15*unitSum((int)(999/15));
+    
+    printf("Sum by Brute Force: %d\n", loopSum);
+    printf("Sum by using formula: %d\n", formulaSum);
 }
